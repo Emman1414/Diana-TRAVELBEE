6964990f23a7a20007ab8b44deadeb9fa836f73c
Emman1414 <0321-2818@lspu.edu.ph>
2024-10-09 23:10:52 +0800

    justnewnew

--- js/slider.js
+++ js/slider.js
@@ -1,45 +1,30 @@
 // var slider = tns({
 //   container: ".image__slider",
 //   items: 6,
+//   speed: 200,
 //   slideBy: "1",
 //   autoplay: false,
-//   controlsContainer: "#controls",
+//   containerControls: "#controls",
 //   prevButton: ".prev",
 //   nextButton: ".next",
 //   nav: false,
-//   speed: 300,
-//   responsive: {
-//     1024: {
-//       items: 6,
-//     },
-//     520: {
-//       items: 2,
-//     },
-//     480: {
-//       items: 1,
-//     },
-//   },
 // });
 
 var slider = tns({
   container: ".image__slider",
   items: 6,
   speed: 200,
-  gutter: 100,
   slideBy: "1",
-  autoplay: false,
-  containerControls: "#controls",
+  autoplay: true,
+  controlsContainer: "#controls",
   prevButton: ".prev",
   nextButton: ".next",
   nav: false,
   responsive: {
-    1024: {
+    1250: {
       items: 6,
     },
-    520: {
-      items: 2,
-    },
-    480: {
+    200: {
       items: 1,
     },
   },
